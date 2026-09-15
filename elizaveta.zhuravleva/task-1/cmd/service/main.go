74f0b81c1b7c700665b7ef7f53f20858b72c620c
elizazha <lizokbella@mail.ru>
2026-09-15 21:39:48 +0300

    [TASK-1] read and validate operands

--- elizaveta.zhuravleva/task-1/cmd/service/main.go
+++ elizaveta.zhuravleva/task-1/cmd/service/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var first, second int
+
+	_, err := fmt.Scanln(&first)
+	if err != nil{
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&second)
+	if err != nil{
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	fmt.Println(first, second)
 }
